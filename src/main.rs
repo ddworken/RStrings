0f959d9d2ac65ff8e019c9477ca56bd06089b2a8
David Dworken <david@daviddworken.com>
2015-10-11 23:21:37 -0400

    Switched to docopt for argument parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+extern crate docopt;
+use docopt::Docopt;
+
+extern crate rustc_serialize;
+
 use std::io; //for stdin
 use std::env; 
 use std::path::Path; //for filenames 
@@ -5,18 +10,45 @@ use std::fs::File; //for the file
 use std::io::BufReader; //buffered reader so we can handle large files
 use std::io::Read; //to read from the above file
 
+const USAGE: &'static str = "
+Usage: rustStrings [options] [<file>]
+
+Options:
+    -b, --bytes=<num>  set the number of printable bytes needed for something to qualify as a string [default: 4]
+    -n, --nullbytes  set to disable the null byte requirement
+    -f, --filename  print the name of the file before each line
+    -l, --location  print the location of the string in the binary (bytes past starting point)
+    -h, --help  display this help and exit
+    -v, --version  output version information and exit
+";
+
+#[derive(Debug, RustcDecodable)]
+struct Args {
+    arg_file: String,
+    flag_bytes: i32,
+    flag_nullbytes: bool,
+    flag_filename: bool,
+    flag_location: bool,
+    flag_help: bool,
+    flag_version: bool,
+}
+
 fn main(){
-    let mut args: Vec<_> = env::args().collect(); //get all the arguments
-    args.remove(0); //remove the first one b/c the first one is the filename
-    if args.len() != 1 { 
-        panic!("This program accepts 1 argument, you supplied it with {} arguments", args.len());
+    let args: Args = Docopt::new(USAGE)
+        .and_then(|d| d.decode())
+        .unwrap_or_else(|e| e.exit());
+    let filename = args.arg_file.clone();
+
+    if args.flag_version {
+        const VERSION: &'static str = env!("CARGO_PKG_VERSION");
+        println!("RStrings {}", VERSION);
+        std::process::exit(0);        
     }
-    let filename = args[0].clone(); //clone() b/c we should leave args intact
 
     println!("Opening {} to search it for strings...", filename);   
     let file = openFile(filename.clone());
     println!("Opened {}. ", filename);
-    searchFile(file);
+    searchFile(file, args.flag_bytes, args.flag_nullbytes, args.flag_filename, filename.clone(), args.flag_location);
 }
 
 fn isPrintable(char: u8) -> bool { //checks whether the character (as a u8) is printable
@@ -26,7 +58,7 @@ fn isPrintable(char: u8) -> bool { //checks whether the character (as a u8) is p
     return false;
 }
 
-fn checkForString(file: Vec<u8>, index: usize) -> (bool, u64) { //bool is whether or not we did, u64 is the length of it if we did
+fn checkForString(file: Vec<u8>, index: usize, numBytes: i32, nullBytes: bool) -> (bool, u64) { //bool is whether or not we did, u64 is the length of it if we did
     let mut isFound = false; //by default we never found it
     let mut size = 0;   //size=0 is the default
     let mut i = 0;  //used in the loop{} structure as a counter
@@ -50,14 +82,14 @@ fn checkForString(file: Vec<u8>, index: usize) -> (bool, u64) { //bool is whethe
     return (isFound, size as u64)   //return it as a u64 so it is sufficiently large
 }
 
-fn searchFile(file: Vec<u8>) { //given a vector of u8 will search the file
+fn searchFile(file: Vec<u8>, numBytes: i32, nullBytes: bool, printFile: bool, filename: String, printLocation: bool) { //given a vector of u8 will search the file
     let mut numToSkip = 0;  //the number to skip (used when we find a 5 character string so we don't then print a 4 character string followed by a 3 character and so on
     for (index,char) in file.iter().enumerate() { //index,char b/c we need both
         if numToSkip > 0 { //if we need to skip, do so
             numToSkip -= 1; //decrement it so we don't skip forever 
         }
         else { //if not skipping: 
-            let temp = checkForString(file.clone(), index); //temp is a tuple; temp.0 is whether or not we found one; temp.1 is the length of the string we found 
+            let temp = checkForString(file.clone(), index, numBytes, nullBytes); //temp is a tuple; temp.0 is whether or not we found one; temp.1 is the length of the string we found 
             if temp.0 { //if temp.0 is true then we found a string
                 println!("{}", getString(file.clone(), index as u64, index as u64+temp.1));
                 numToSkip = temp.1; //now we need to skip the length of the string
